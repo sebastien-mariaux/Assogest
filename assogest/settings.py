8e2ae21245c1e1919091d7b617722f37f788e469
¨Sebastien <sebastien.mariaux@pm.me>
2025-01-20 19:26:51 +0100

    Use Postgresql

--- assogest/settings.py
+++ assogest/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'assogest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': 'django123',
+        'HOST': 'localhost',
+        'PORT': 5434,
     }
 }
 
@@ -163,3 +167,7 @@ COMPRESS_OFFLINE = True
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'tmp', 'emails')
+DEFAULT_FROM_EMAIL = 'contact@assogest.fr'
