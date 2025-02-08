8b8c4b84a7302954fd6dc99595f5b967a0ba27b9
¨Sebastien <sebastien.mariaux@pm.me>
2025-02-08 14:05:39 +0100

    Test new FE

--- organization/urls.py
+++ organization/urls.py
@@ -4,6 +4,6 @@ from .views import OrganizationsListView, OrganizationDetailView
 app_name = 'organization'
 
 urlpatterns = (
-    path('', OrganizationsListView.as_view(), name='organizations'),
+    path('', OrganizationsListView.as_view(), name='organization-list'),
     path('<slug:slug>/', OrganizationDetailView.as_view(), name='organization-detail'),
 )
