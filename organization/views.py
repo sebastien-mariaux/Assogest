e074d9ffb81ec9f173392bf5a14a13a3420c1406
¨Sebastien <sebastien.mariaux@pm.me>
2025-02-08 14:49:21 +0100

    Improve org list

--- organization/views.py
+++ organization/views.py
@@ -1,5 +1,6 @@
 
 # Create your views here.
+from django.db.models import Count
 from django.views.generic import ListView, DetailView
 from .models import Organization
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -7,16 +8,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class OrganizationsListView(ListView, LoginRequiredMixin):
     model = Organization
-    context_object_name = 'organizations'
     paginate_by = 10
     ordering = ['name']
 
     def get_queryset(self):
-        return (
-            Organization.objects
-            .filter(members=self.request.user.member)
-            .order_by('name')
-        )
+        return Organization.objects.annotate(
+            members_count=Count('members')
+        ).order_by('-created_at')
 
     # TODO: if only one organization, redirect to organization detail view
 
@@ -37,4 +35,3 @@ class OrganizationDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['events'] = self.object.events.order_by('start_time')[:5]
         return context
-    
